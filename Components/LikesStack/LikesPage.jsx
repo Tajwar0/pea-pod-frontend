084bd1ea725dc4ee0ae5e83f5e6bed70d647dbba
Tajwar0 <tajwar_h@hotmail.com>
2022-08-17 16:35:03 +0100

    likes page component - implement dynamically updated likes page

--- Components/LikesStack/LikesPage.jsx
+++ Components/LikesStack/LikesPage.jsx
@@ -1,6 +1,5 @@
 import { useState, useRef, useEffect, useContext } from "react";
 import { View, FlatList, StyleSheet, Animated } from "react-native";
-import slides from "../../assets/slides";
 import LikesItem from "./LikesItem";
 import { UserContext } from "../../Contexts/User";
 export default function LikesPage({ navigation }) {
@@ -11,19 +10,18 @@ export default function LikesPage({ navigation }) {
   const viewableItemsChanged = useRef(({ viewableItems }) => {
     setCurrentIndex(viewableItems[0].index);
   }).current;
-  
+
   const viewConfig = useRef({ viewAreCoveragePercentThreshold: 50 }).current;
 
   useEffect(() => {
-    console.log(userName);
     const getUserMatches = async () => {
       try {
         const response = await fetch(
-          `https://pea-pod-api.herokuapp.com/user/${userName}/incoming_likes`
+          `https://pea-pod-api.herokuapp.com/user/${userName.userName}/incoming_likes`
         );
         const json = await response.json();
         setUserMatches(json);
-        console.log(json);
+        console.log(userName);
       } catch (error) {
         console.error(error);
       }
